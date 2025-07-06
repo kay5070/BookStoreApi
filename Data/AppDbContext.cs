c849bb87bf2a553ba9f1de098a20c5230321ea14
kay5070 <karemsabry5@gmail.com>
2025-07-06 18:52:15 +0300

    Installed EF Core InMemory provider and configured database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookStoreApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Data;
+
+public class AppDbContext :DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Book> Books => Set<Book>();
+    
+}
