22f99f785adeb45af621f724f96e595cc786e1fb
kay5070 <karemsabry5@gmail.com>
2025-07-11 23:47:43 +0300

    added unit tests for the crud operations

--- BookStoreApi/Data/AppDbContext.cs
+++ BookStoreApi/Data/AppDbContext.cs
@@ -8,6 +8,6 @@ public class AppDbContext :DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-    public DbSet<Book> Books => Set<Book>();
+    public virtual DbSet<Book> Books => Set<Book>();
     
 }
